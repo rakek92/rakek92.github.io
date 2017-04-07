78df98ffb3f4f3bb9f90b59b8658c20af5648c78
Mishev_Ihor <vbitd92@gmail.com>
2017-04-07 15:36:40 +0300

    ok

---
+++ README.md
@@ -0,0 +1,7 @@
+## About this repo
+This project is written using pure JS (see `scripts/main.js`) 
+
+## What is inside
+Just list of works (each of them can be found in `/works`)
+
+[Demo on github pages](http://mkozhukharenko.github.io/).
---
+++ scripts/data.js
@@ -0,0 +1,92 @@
+var data = [
+	{	
+		id: 1,
+		title: "World time map",
+		description: "Check time/lng/lat/timezone on Global map (Leaflet). Build using MVC pattern. Timezonedb API used for getting local time by lng/lat.",
+		linkToDemo: "http://mkozhukharenko.github.io/world-time-map/app/index.html",
+		linkToCode: "https://github.com/mkozhukharenko/world-time-map",
+		image: "images/works-color/worl-time-map.jpg",
+		tags: ["Jqeury", "AJAX", "Less"]
+	},
+	{	
+		id: 1,
+		title: "This portfolio",
+		description: "Almost all DOM generates bu JS. Build using MVC pattern. Less preprocessor used",
+		linkToDemo: "http://mkozhukharenko.github.io/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io",
+		image: "images/works-color/this_portfolio.jpg",
+		tags: ["Pure JS", "Pure HTML/CSS", "Less"]
+	},
+	{	
+		id: 1,
+		title: "News picker",
+		description: "Simple app that receives the news from the server (Guardian Open API) and parse it to DOM.",
+		linkToDemo: "http://mkozhukharenko.github.io/newspicker/src/index.html#/news",
+		linkToCode: "https://github.com/mkozhukharenko/newspicker",
+		image: "images/works-color/newspicker.jpg",
+		tags: ["Angular", "Bootstrap", "AJAX"]
+	},
+	{	
+		id: 2,
+		title: "Questionnary",
+		description: "Multi parent graph presented as a complex questionnaire. This's the solution for qualifying round of Web Challenge 2015.",
+		linkToDemo: "http://mkozhukharenko.github.io/multi-parent-graph/",
+		linkToCode: "https://github.com/mkozhukharenko/multi-parent-graph",
+		image: "images/works-color/graph.jpg",
+		tags: ["Pure JS"]
+	},
+	{	
+		id: 3,
+		title: "Form validation",
+		description: "Form validation using as registration form example. Field 'email' has server side validation.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/form-validation/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/form-validation",
+		image: "images/works-color/form-validation.jpg",
+		tags: ["AJAX", "Bootstrap", "Pure JS"]
+	},
+	{	
+		id: 4,
+		title: "Store mock up",
+		description: "Home page mock up for online store. None css frameworks has used.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/electronic-shop/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/electronic-shop",
+		image: "images/works-color/shop.jpg",
+		tags: ["Pure HTML/CSS"]
+	},
+	{	
+		id: 5,
+		title: "Timer",
+		description: "Several timers can be inserted on one page: code has bulid on prototypes. No frameforks.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/timer",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/timer",
+		image: "images/works-color/timer.jpg",
+		tags: ["Pure JS"]
+	},
+	{	
+		id: 6,
+		title: "Zoomable image",
+		description: "Fully adaptive zoomable image (fit all screens). Code is reusable.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/zoomable/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/zoomable",
+		image: "images/works-color/zoomable.jpg",
+		tags: ["jQuery", "Pure HTML/CSS"]
+	},
+	{	
+		id: 7,
+		title: "Slider",
+		description: "Can be reusable (several sliders on one page). Slide fit all the screens. Build using MPC pattern.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/slider/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/slider",
+		image: "images/works-color/slider.jpg",
+		tags: ["jQuery", "Pure HTML/CSS"]
+	},
+	{	
+		id: 8,
+		title: "Portfolio mock up",
+		description: "Mock up for portfolio. None css frameworks has used.",
+		linkToDemo: "http://mkozhukharenko.github.io/works/portfolio/",
+		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/portfolio",
+		image: "images/works-color/portfolio.jpg",
+		tags: ["Pure HTML/CSS"]
+	}
+];
